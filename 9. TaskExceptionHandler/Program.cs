dea0eb253f8f04e1917616120f12d54ccb8117af
Ion Gritco <ion.gritco@gmail.com>
2018-09-02 16:04:35 +0300

    :Added exception handler example

---
+++ 9. TaskExceptionHandler/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskExceptionHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Task task = Task.Run(action: DoSomething);
+                task.Wait();
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine($"Catched the error: {ex.InnerException}");
+            }
+
+            Console.WriteLine("Finished job");
+        }
+
+        private static void DoSomething()
+        {
+            throw new Exception();
+        }
+    }
+}
